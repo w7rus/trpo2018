e5d8ed918e31990cc9b31b6c51555fa7578c1eb2
artiom46 <artor663@gmail.com>
2018-06-20 21:01:51 +0700

    input cycle

--- src/nameFile.c
+++ src/nameFile.c
@@ -4,11 +4,13 @@
 #include <string.h>
 
 void TestFile(){
+	int StopEnter = 0;
 	char nameFile[80];
 	char nameTest[80];
-	char timeTest[40];
+	int timeTest = 0;
 	char Question[280];
 	char Answer[280];
+	char shadow[0];
 	
 	printf("Enter the name of file: ");
 	
@@ -29,16 +31,25 @@ void TestFile(){
 		fprintf (ptrFile, "NAME=%s\n",nameTest);
 		
 		printf("Time test: ");
-		gets(timeTest);
-		fprintf (ptrFile, "TIME=%s\n", timeTest);
+		scanf("%d", &timeTest);
+		fprintf (ptrFile, "TIME=%d\n", timeTest);
+		
+		while(!StopEnter){
 			
-		printf("Question: ");
-		gets(Question);
-		fprintf (ptrFile, "QUESTION=%s\n",Question);
+			gets(shadow);
 				
-		printf("Answer: ");
-		gets(Answer);
-		fprintf (ptrFile, "ANSWER=%s\n",Answer);
+			printf("Question: ");
+			gets(Question);
+			fprintf (ptrFile, "QUESTION=%s\n",Question);
+					
+			printf("Answer: ");
+			gets(Answer);
+			fprintf (ptrFile, "ANSWER=%s\n",Answer);
+			
+			printf("(Continue typing: yes - 0, no - 1): ");
+			scanf("%d", &StopEnter);
+			
+		}	
 	}
 	
 	else
